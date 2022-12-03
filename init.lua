0ade59bef6ee95e8ea667395e0e50f484a5484d8
cborja36 <cborja36@gmail.com>
2022-12-03 17:50:23 +0100

    better header + github copilot

--- init.lua
+++ init.lua
@@ -4,29 +4,61 @@
 -- You can think of a Lua "table" as a dictionary like data structure the
 -- normal format is "key = value". These also handle array like data structures
 -- where a value with no key simply has an implicit numeric key
-local config = {
-  -- set vim options here (vim.<first_key>.<second_key> = value)
-  options = {
-    opt = {
-      -- set to true or false etc.
-      relativenumber = false, -- sets vim.opt.relativenumber
-    },
-  },
 
-  -- Set dashboard header
-  header = {
-    " █████  ███████ ████████ ██████   ██████",
-    "██   ██ ██         ██    ██   ██ ██    ██",
-    "███████ ███████    ██    ██████  ██    ██",
-    "██   ██      ██    ██    ██   ██ ██    ██",
-    "lll█   ██ ███████    ██    ██   ██  ██████",
-    " ",
-    "    ███    ██ ██    ██ ██ ███    ███",
-    "    ████   ██ ██    ██ ██ ████  ████",
-    "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
-    "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
-    "    ██   ████   ████   ██ ██      ██",
-  },
+local config = {
+
+        -- If you need more control, you can use the function()...end notation
+        options = function(local_vim)
+
+                -- GitHub Copilot
+                local_vim.g.copilot_no_tab = " "
+
+                return local_vim
+        end,
+
+        plugins = {
+                init = {
+                        { "github/copilot.vim" },
+                }
+        },
+
+        lsp = {
+
+                -- override the mason server-registration function
+                server_registration = function(server, opts)
+                        require("lspconfig")[server].setup(opts)
+                end,
+
+                -- Add overrides for LSP server settings, the keys are the name of the server
+                ["server-settings"] = {
+                        pyright = {
+                                settings = {
+                                        python = {
+                                                analysis = {
+                                                        typeCheckingMode = "off"
+                                                }
+                                        }
+                                }
+                        }
+                },
+        },
+
+        header = {
+                " █████╗ ███████╗████████╗██████╗  ██████╗ ",
+                "██╔══██╗██╔════╝╚══██╔══╝██╔══██╗██╔═══██╗",
+                "███████║███████╗   ██║   ██████╔╝██║   ██║",
+                "██╔══██║╚════██║   ██║   ██╔══██╗██║   ██║",
+                "██║  ██║███████║   ██║   ██║  ██║╚██████╔╝",
+                "╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝ ",
+                "                                          ",
+                "    ███╗   ██╗██╗   ██╗██╗███╗   ███╗     ",
+                "    ████╗  ██║██║   ██║██║████╗ ████║     ",
+                "    ██╔██╗ ██║██║   ██║██║██╔████╔██║     ",
+                "    ██║╚██╗██║╚██╗ ██╔╝██║██║╚██╔╝██║     ",
+                "    ██║ ╚████║ ╚████╔╝ ██║██║ ╚═╝ ██║     ",
+                "    ╚═╝  ╚═══╝  ╚═══╝  ╚═╝╚═╝     ╚═╝     ",
+        },
+
 }
 
 return config
